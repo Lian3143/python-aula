9367fcdd3108e83588f2e6bf63f576692284ea72
Lian <ithaloxavierlian@gmail.com>
2023-11-30 04:33:58 -0300

    arquivo de teste modificado

--- code/teste.py
+++
@@ -1,6 +0,0 @@
-def KmMilha (km):
-   milha = km * 0.621371
-   return milha
-
-resultado = KmMilha(float(input('Digite um valor em quilômetros: ')))
-print('Equivalente em milhas: {}' . format(resultado))
