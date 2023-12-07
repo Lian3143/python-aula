ec2f228dbb4f84b2b538f6c0ecae29fe6ce44a8a
Lian <ithaloxavierlian@gmail.com>
2023-12-07 12:43:13 -0300

    Update teste.py

--- code/atividades em python/teste.py
+++ code/atividades em python/teste.py
@@ -1,6 +1,1 @@
-#07 - Faça um programa que leia a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m**2 
-
-larg = float(input('Digite a largura da parede: '))
-alt = float(input('Digite a altura da parede: '))
-area = larg * alt
-print(f'A sua parede tem a dimensão de {larg}x{alt}, e uma área de {area}.\nPara pintar essa parede, você precisará de {area/2} litros de tinta.')
+#10 - Escreva um programa que converta  uma temperatura digitada em °C e converta para °F
