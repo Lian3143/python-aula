35ab73c29b5330acce493944f759aeba1badc745
Lian <ithaloxavierlian@gmail.com>
2023-12-07 10:55:14 -0300

    respondi a questão 07

--- code/atividades em python/aula-02.py
+++ code/atividades em python/aula-02.py
@@ -61,7 +61,12 @@ calc = qnt/3.27
 
 print('Se você possui {} reais, então você poderá comprar {:.2f} dólares.' .format(qnt, calc ))
 
-#07 - Faça um programa que leia a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m**2
+#07 - Faça um programa que leia a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m**2 
+
+larg = float(input('Digite a largura da parede: '))
+alt = float(input('Digite a altura da parede: '))
+area = larg * alt
+print(f'A sua parede tem a dimensão de {larg}x{alt}, e uma área de {area}.\nPara pintar essa parede, você precisará de {area/2} litros de tinta.')
 
 #08 - Faça um algoritmo que leia o preço de um produto e mostre o novo preço, com 5% de desconto.
 preco = float(input('Digite o valor do produto: '))
--- code/atividades em python/teste.py
+++ code/atividades em python/teste.py
@@ -1,5 +1,6 @@
-#09 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-salario = float(input('Digite o valor do seu salário: '))
-aumento = salario * 0.05
+#07 - Faça um programa que leia a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m**2 
 
-print(f'O seu salário atual é {salario}.\nCom o aumento de 5%, ficará {aumento}.')
+larg = float(input('Digite a largura da parede: '))
+alt = float(input('Digite a altura da parede: '))
+area = larg * alt
+print(f'A sua parede tem a dimensão de {larg}x{alt}, e uma área de {area}.\nPara pintar essa parede, você precisará de {area/2} litros de tinta.')
