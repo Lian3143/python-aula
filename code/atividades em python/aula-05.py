6e0cbbbd4654bce7cfc4044868700dfe4fc1d8aa
Lian <ithaloxavierlian@gmail.com>
2023-12-14 20:33:25 -0300

    ATIVIDADES

---
+++ code/atividades em python/aula-05.py
@@ -0,0 +1,41 @@
+#CONDIÇÕES SIMPLES E ANINHADAS 
+#IF E ELSE
+
+#exemplo
+condicao = ''
+if condicao.esquerda():
+    print(f'condicao')
+
+else: 
+    print(f"condicao")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#AULA PRÁTICA! 
+
+n1 = float(input('Digite a sua primeira nota: '))
+n2 = float(input('Digite a sua segunda nota: '))
+m = (n1 + n2)/2
+
+if m >= 6.0:
+    print('Sua nota está boa! Parabéns')
+else:
+    print("Sua nota está ruim! Estude mais")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#ATIVIDADES | DESAFIOS
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#01 - Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e faça o usuário tentar advinhar qual foi o número escolhido. O programa deverá escrever na tela se o aluno acertou ou não. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#02 - Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre a mensagem dizendo que ele foi multado. A multa va icustar R$7.00 reais por cada KM acima do limite. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#03 - Crie um programa que leia um numero inteiro e mostre na tela se ele é par ou ímpar. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#04 - Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para viagens de até 200km e R$0.45 para viagens mais longas. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#05 - Faça um programa que leia um ano qualquer e mostre se ele é bissexto. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#06 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#07 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1.250,00, calcule um aumento de 10%. Para inferiores ou iguais, o aumento é de 15%. 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#08 - Desenvolva um programa que leia o comprimento de três rotas e diga ao usuário se elas podem ou não formar um triângulo.
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- code/atividades em python/main.py
+++ code/atividades em python/main.py
@@ -1,3 +1,2 @@
-#CONDIÇÕES SIMPLES E ANINHADAS 
-#IF E ELSE
+#01 - Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e faça o usuário tentar advinhar qual foi o número escolhido. O programa deverá escrever na tela se o aluno acertou ou não. 
 
