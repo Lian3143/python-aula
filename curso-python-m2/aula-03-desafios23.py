52cad045e19d8e19c1abb986f564392afdfd23f9
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-12 15:11:51 -0300

    .

--- curso-python-m2/aula-03-desafios23.py
+++ curso-python-m2/aula-03-desafios23.py
@@ -1,2 +1,3 @@
 #2 - Melhore o desafio028 onde o computador vai "pensar" em um número de entre 0 e 10.
 # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
