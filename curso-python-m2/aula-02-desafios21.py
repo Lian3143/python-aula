ea2175f5e06cfd3b3a68489f312f67c4e3a3a1ba
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-12 10:26:56 -0300

    fim da aula 2.
    RESOLVI TODAS AS QUESTÕES DA AULA 2. COMEÇANDO AULA 3, SOBRE LAÇOS COM WHILE

--- curso-python-m2/aula-02-desafios21.py
+++ curso-python-m2/aula-02-desafios21.py
@@ -1,8 +1,9 @@
-#11 - Desenvolva um program que leia nome, idade e sexo de 4 pessoas. No final, mostre: 
+#11 - Desenvolva um program que leia nome, idade e sexo de 4 pessoas. No final, mostre:
 #A média de idade do grupo
 #Qual o nome do homem mais velho.
 #Quantas mulheres tem menos 20 anos
-#-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-
+#-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-=--=--=--=--=--=--=--=--=--=--=-
+
 #variáveis de apoio
 mais_velho = 0
 nome_mais_velho = ''
@@ -12,7 +13,8 @@ idade_total = 0
 
 #estrutura de repetição.
 for i in range(1, 5):
-    nome = str(input('Digite o {i}° nome: ')).strip()
+    print(f'=--=- {i}ª Pessoa -=--=')
+    nome = str(input(f'Digite o {i}° nome: ')).strip()
     idade = int(input(f'Digite a idade da {i}ª pessoa: '))
     sexo = str(input(f'Digite o sexo da {i}ª pessoa: ')).strip().upper()
 
@@ -27,10 +29,21 @@ for i in range(1, 5):
 #verificando o nome do homem mais velho
     if sexo == 'M':
         mais_velho == idade
-    if idade >= mais_velho:
+
+    if sexo == 'M' and idade >= mais_velho:
         mais_velho = idade
         nome_mais_velho = nome
 
+#Print com o resultado
+print('-=-'*20)
 
+print(f' A média de idade do grupo é {media_idade:.2f} anos.'
+      f'\n O nome do homem mais velho é {nome_mais_velho} e ele possui {mais_velho} anos.')
+if mulheres_menos_20 == 0:
+    print(f' Nenhuma mulher cadastrada possui 20 anos ou menos. ')
+elif mulheres_menos_20 == 1:
+    print(f' Apenas uma mulher possui 20 anos ou menos.')
+else:
+    print(f' Um total de {mulheres_menos_20} mulheres possuem menos de 20 anos. ')
 
-print(f' A média de idade do grupo é: {media_idade:.2f}.\n O nome do homem mais velho é {nome_mais_velho} anos.\n E {mulheres_menos_20} possuem menos de 20 anos. ')
+print('-=-' * 20)
