784592701710dfa946b0ab3db7002fc03b34567e
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-12 08:14:44 -0300

    .

--- curso-python-m2/aula-02-desafio20.py
+++ curso-python-m2/aula-02-desafio20.py
@@ -1,9 +1,6 @@
 #10 - Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
 #-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
-#Quantidade de pessoas
-
-pessoas = 5
 
 #Var de apoio
 
@@ -14,7 +11,9 @@ for i in range(1, pessoas+1):
     peso = float(input(f'Digite o peso da {i}ª pessoa: '))
 
 #verificando o maior e o menor peso
-    
+if i == 1:
+    maior_peso = peso
+    menor_peso = peso
     
     
 #print com o resultado
