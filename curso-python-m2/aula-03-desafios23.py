e861075de04a487e2555e676da9bc5b3fb77cba4
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-12 19:49:27 -0300

    .

--- curso-python-m2/aula-03-desafios23.py
+++ curso-python-m2/aula-03-desafios23.py
@@ -1,3 +1,38 @@
 #2 - Melhore o desafio028 onde o computador vai "pensar" em um número de entre 0 e 10.
 # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
+from random import choice
+
+#variáveis de apoio
+maquina_jogadas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+escolha_maquina = choice(maquina_jogadas) #aleatoriezando a escolha da máquina
+jogador_escolha = 0
+jogada_inicial = int(input('Você começa! De 1 a 10, qual número você acha que eu escolhi? '))
+print(escolha_maquina)
+erros = 0 #total de palpites do jogador
+
+
+#laço de repetição
+while jogador_escolha != escolha_maquina:
+    if jogador_escolha != maquina_jogadas:
+        print('Número inválido. Por favor, tente novamente utilizando apenas números inteiros de 1 à 10. ☺')
+
+    #verificando se o jogador errou. Se errou, irá repetir o loop até acertar..
+    print(f'Você errou! Tente novamente!')
+    jogador_escolha = int(input('Qual será o seu próximo palpite? '))
+    erros += 1
+
+    # verificando se o jogador ganhou
+    if jogada_inicial == escolha_maquina or jogador_escolha == escolha_maquina:
+        print(f' Parabéns, você venceu!')
+        if erros < 3:
+            print(f'Você precisou de apenas {erros} para acertar a minha escolha! Você é bom nisso!')
+
+        elif erros < 6:
+            print(f'{erros} jogadas? Você até que é bom. ')
+
+        elif erros >= 10:
+            print(f'{erros} jogadas? Acho que você precisa praticar um pouquinho...')
+
+#print para encerrar o programa.
+print('Obrigado por participar do nosso jogo! Volte sempre que quiser! ♥\nEncerrando programa...')
--- curso-python-m2/aula-03-desafios24.py
+++ curso-python-m2/aula-03-desafios24.py
@@ -4,5 +4,14 @@
 #3 - maior
 #4 - novos números
 #5 - ecerrar programa
-
 #Seu programa deverá realizar a operação solicitada em cada caso.
+
+
+#Lendo dois valores.
+a = 0
+
+for i in range(1, 3):
+    x = int(input(f'Digite o {i}° valor: '))
+    a =+ x
+
+
