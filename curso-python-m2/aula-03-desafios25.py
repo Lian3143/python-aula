d456c318952832657604bc2860b174405323763d
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-13 18:31:16 -0300

    .

--- curso-python-m2/aula-03-desafios25.py
+++ curso-python-m2/aula-03-desafios25.py
@@ -1,5 +1,23 @@
 #4 - Faça um programa que leia um número qualquer e mostre o seu fatorial.
 #Ex: 5! = 5x4x3x2x1 = 120
 
-from math import factorial
+#resolvendo utilizando uma biblioteca
+'''from math import factorial
+x = int(input('Digite um valor para descobrir o seu fatorial: '))
+f = factorial(x)
+print(f'{x}! é igual a: {f}')'''
 
+#resolvendo sem biblioteca
+
+x = int(input('Digite um valor para descobrir o seu fatorial: '))
+
+c = x
+f = 1
+
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' =', end='')
+    f *= c
+    c -= 1
+
+print(f)
