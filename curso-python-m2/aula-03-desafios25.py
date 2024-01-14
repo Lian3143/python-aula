8f980abe755511729de169f128e810ebf3f831c2
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-13 22:28:42 -0300

    .

--- curso-python-m2/aula-03-desafios25.py
+++ curso-python-m2/aula-03-desafios25.py
@@ -2,22 +2,64 @@
 #Ex: 5! = 5x4x3x2x1 = 120
 
 #resolvendo utilizando uma biblioteca
-'''from math import factorial
+from math import factorial
 x = int(input('Digite um valor para descobrir o seu fatorial: '))
 f = factorial(x)
-print(f'{x}! é igual a: {f}')'''
+print(f'{x}! é igual a: {f}')
 
-#resolvendo sem biblioteca
+#Resolvendo utilizando WHILE e sem biblioteca
 
 x = int(input('Digite um valor para descobrir o seu fatorial: '))
 
-c = x
+c = x #variável para um contador
 f = 1
-
+print(f'{x}! = ', end='')
 while c > 0:
     print(f'{c}', end='')
     print(' x ' if c > 1 else ' =', end='')
     f *= c
     c -= 1
 
-print(f)
+print(f' {f}', end='')
+
+#Resolvendo utilizando o for e sem biblioteca
+
+x = int(input('Digite o valor que deseja descobrir o fatorial: '))
+c = x
+f = 1
+
+print(f'{x}! = ', end='')
+
+for i in range(0, c):
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' =', end='')
+    f *= c
+    c -= 1
+
+print(f' {f}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- curso-python-m2/aula-03-desafios26.py
+++ curso-python-m2/aula-03-desafios26.py
@@ -1,1 +1,28 @@
 #5 - Refaça o DESAFIO51, lendo o primeiro termo e a razãp de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+#-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-
+
+#UTILIZANDO O FOR
+p = int(input('Primeiro termo: ')) #primeiro termo
+r = int(input('Razão: ')) #razão
+d = p + (10 - 1) * r  #decimo termo
+
+for i in range(p, d + r, r):
+    print(f'{i}', end=' ')
+    print(' =>', end=' ')
+
+print('Acabou. \nEncerrando programa... ')
+
+
+
+#RESOLUÇÃO UTILIZANDO O WHILE
+p = int(input('Primeiro termo: '))
+r = int(input('Digite a razão: '))
+
+termo = p
+cont = 1
+
+while cont <= 10:
+    print(f'{termo} → ', end=' ')
+    termo += r
+    cont += 1
+print(f'Fim.')
