7d8fdf530d888e6e5583c90155b1b8ae2f0e0403
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-15 19:50:34 -0300

    finalizado as atividades sobre while e for.
    Começando a nova aula em breve.

--- curso-python-m2/aula-03-desafios28.py
+++ curso-python-m2/aula-03-desafios28.py
@@ -1,1 +1,21 @@
 #7 - Escreva um programa que leia um número n inteiro e mostre na tela os n primeiros elementos de uma sequência fibonacci.
+
+x = int(input('Quantos termos você quer ver dentro da sequência fibonacci: '))
+
+
+cont = 1 #contador
+a = 0 #primeiro termo fixo
+b = 1 #segundo termo fixo
+c = 0 #terceiro termo, que será a soma do primeiro com o segundo termo
+print(f'{a} → {b} → ', end='')
+#estrutura de repetição
+while cont < x+1:
+    c = a + b
+    print(f'{c} → ', end='')
+    #para realizar a soma, o programa fará o primeiro termo receber o valor do segundo, e o segundo o do terceiro, somando-os entre si e alterando o seu valor até o contador chegar ao seu final.
+    a = b
+    b = c
+    #contador receberá o seu valor (1) e somará 1 toda vez que o loop for repetido.
+    cont += 1
+
+print(f'Fim. ')
--- curso-python-m2/aula-03-desafios29.py
+++ curso-python-m2/aula-03-desafios29.py
@@ -1,2 +1,16 @@
 #8 - Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 # No final, mostre quantos números foram digitados e qual foi a soma entre eles. (Desconsiderando o flog.)
+
+cont = 0
+soma = 0
+x = 0
+
+#loop
+while x != 999:
+
+    x = int(input('Digite um valor: '))
+    cont += 1 #toda vez que o loop for realizado o contador receberá +1
+    soma += x #vai somar os valores digitados e armazenar na variável "soma"
+
+print(f'Foram digitados {cont-1} números.\nSoma dos números digitados é igual a {soma-999}')
+
