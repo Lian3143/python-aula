269bff04463dbfa551b2eda258f3385a2ec9d38a
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-16 10:05:58 -0300

    Arquivos com as atividades criados.

---
+++ curso-python-m2/aula-04.py
@@ -0,0 +1,7 @@
+#AULA 04 DO MUNDO 2 - ABORDANDO INTERRUPÇÃO DE ESTRUTURAS WHILE
+#REPEAT SENDO SIMULADO EM PYTHON. NÃO HÁ UMA ESTRUTURA REPEAT, ENTÃO É NECESSÁRIO "REPLICÁ-LO" DE OUTRA MANEIRA.
+
+#COMANDO BREAK TEM A FUNÇÃO DE SAIR DO LAÇO DE REPETIÇÃO.
+
+#Loop infinito é determinado da seguinte forma: while True. Quando utilizado, ele pode ser interrompido apenas utilizando o comando "BREAK".
+
