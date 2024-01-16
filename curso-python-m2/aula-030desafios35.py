e782bf8ad8ed8f0f621ef89a2a7559d149c4bdcb
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-16 10:06:04 -0300

    Arquivos com as atividades criados.

---
+++ curso-python-m2/aula-030desafios35.py
@@ -0,0 +1,5 @@
+#5 - crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre:
+
+#a) qual o total gasto na compra
+#b) quantos produtos custam mais de R$ 1000.
+#c) qual é o nome do protudo mais barato.
---
+++ curso-python-m2/aula-030desafios36.py
@@ -0,0 +1,3 @@
+#6 - crie um programa que simule o funcionamento de um caixa eletronico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+
+#OBS: CONSIDERAR QUE O CAIXA POSSUI CÉDULAS DE R$ 50, R$20, R$10, R$1.
