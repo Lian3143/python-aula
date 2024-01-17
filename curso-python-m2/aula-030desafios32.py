9656cc113ef21ab34eb44b90d7e55083c445b3b9
Lian <112729994+Lian3143@users.noreply.github.com>
2024-01-17 08:34:57 -0300

    .

--- curso-python-m2/aula-030desafios32.py
+++ curso-python-m2/aula-030desafios32.py
@@ -1,1 +1,20 @@
-#2 - Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+#2 - Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
+# O programa será interrompido quando o número solicitado for negativo.
+
+
+cont = 0
+n = 0
+
+while True:
+    if cont != 11:
+        n2 = int(input('Digite um valor para ver sua tabuada: '))
+    cont += 1
+    if cont < 11:
+        print(f'{n2} x {cont} = {n2 * cont}')
+
+
+    if n2 < 0:
+        break
+
+
+print('Fim.')
--- curso-python-m2/aula-04-desafios31.py
+++ curso-python-m2/aula-04-desafios31.py
@@ -1,1 +1,22 @@
-#1 - crie um programa que leia vários números inteiros pelo teclado. O programa só será interrompido quando o usuário digitar o valor "999", que é  a condição de parada. No final, mostre qunatos números foram digitados e qual foi a soma entre eles. (desconsiderando o flag).
+#1 - crie um programa que leia vários números inteiros pelo teclado.
+# O programa só será interrompido quando o usuário digitar o valor "999", que é  a condição de parada.
+# No final, mostre qunatos números foram digitados e qual foi a soma entre eles. (desconsiderando o flag).
+
+#-=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=
+#variável de apoio
+n = 0
+cont = 0
+soma = 0
+#loop infinito
+
+while True:
+    n = int(input('Digite um valor (para interromper o programa digite "999"): '))
+
+    #definindo o "flag" para parada do loop.
+    if n == 999:
+        break
+    soma += n #somando os valores entre si.
+    cont += 1 #contador.
+
+#print com o resultado.
+print(f'Foram digitados {cont} números e a soma entre eles é: {soma}.')
