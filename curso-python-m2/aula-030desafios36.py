fa7c311ee46019f50312cd07b7e62db8d4a5c59c
Lian <112729994+Lian3143@users.noreply.github.com>
2024-02-18 08:18:38 -0300

    .
    finalizando o mundo2

--- curso-python-m2/aula-030desafios36.py
+++ curso-python-m2/aula-030desafios36.py
@@ -1,3 +1,15 @@
-#6 - crie um programa que simule o funcionamento de um caixa eletronico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+#6 - Crie um programa que simule o funcionamento de um caixa eletronico.
+#No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
 
 #OBS: CONSIDERAR QUE O CAIXA POSSUI CÉDULAS DE R$ 50, R$20, R$10, R$1.
+
+nome = str(input('Digite o seu nome: ')).strip()
+
+print(f'Seja bem-vindo, {nome}!')
+cont = 0
+
+while True:
+    print('Qual operação deseja realizar?\n1 -  ')
+
+
+
